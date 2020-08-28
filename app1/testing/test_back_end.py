8e46d8beec55e6b281a02d03f6ab5641ff20b060
chris-burd91 <chrisburd91@gmail.com>
2020-08-28 16:05:13 +0000

    testing

--- app1/testing/test_back_end.py
+++ app1/testing/test_back_end.py
@@ -2,12 +2,13 @@ import requests
 import unittest
 
 from unittest.mock import patch
+import requests_mock
 
 from flask import url_for
 from flask_testing import TestCase
 from application import app, db
 from application.models import _8Ball
-from os import getenv
+#from os import getenv
 
 class TestBase(TestCase):
 
@@ -23,10 +24,14 @@ class TestViews(TestBase):
 
     def test_8ballpage_view(self):
 
-        with patch('requests.get') as g:
-            with patch('requests.post') as p:
-                g.return_value.text = "A"
-                p.return_value.text = "Well, Duh"
+        with requests_mock.mock() as g:
+            g.get('http://app2:5001/number', json ="A")
+            g.get('http://app3:5002/answer', json ={ "A" : "Well, Duh", "B" : "Yes, If you leave me alone..", "C" : "You wish", "D" : "So now you need my help...?", "E" : "You call that a question?...", "F" : "Trump uses me when he decides to go to war", "G" : "Pluto says no and that he's still a Planet", "H" : "Ask me when I care..", "I": "Get a life you Animal!", "I" : "You shouldn't use me to make your life decisions.."})
+            g.post('http://app4:5003/generate', json = "Well, Duh")
+        #with patch('requests.get') as g:
+        #    with patch('requests.post') as p:
+        #        g.return_value.text = "A"
+        #        p.return_value.text = "Well, Duh"
 
             response = self.client.get(url_for('_8ball'))
             self.assertIn(b"A", response.data)
