5ae3ff21a1482c9c9316e19cd621c45e9e40d66e
chris-burd91 <chrisburd91@gmail.com>
2020-08-29 23:49:44 +0000

    working playbook

--- app1/testing/test_back_end.py
+++ app1/testing/test_back_end.py
@@ -15,7 +15,7 @@ class TestBase(TestCase):
     def create_app(self):
 
         config_name = 'testing'
-        app.config.update(SQLALCHEMY_DATABASE_URI=getenv('TEST_DB_URI'),
+        app.config.update(SQLALCHEMY_DATABASE_URI='http://127.0.0.1:3306',
                TEST_SECRET_KEY=getenv('TEST_SECRET_KEY'),
                 WTF_CSRF_ENABLED=False,
                 DEBUG=True
