48db0efdd8de61ee68418c015d08d9525f13ebcc
chris-burd91 <chrisburd91@gmail.com>
2020-08-31 17:59:20 +0000

    database not being read again Operational error and getting connection refused error

--- app1/application/models.py
+++ app1/application/models.py
@@ -5,5 +5,5 @@ class _8Ball(db.Model):
     order = db.Column(db.String(50))
     answer = db.Column(db.String(50))
 
-    #def __repr__(self):
-    #    return ' '.join(['Order:', self.order, 'Answer:', self.answer])
+    def __repr__(self):
+        return ' '.join(['Order:', self.order, 'Answer:', self.answer])
