3f7388dc3b1696965f1a9f92b3fabe80513d0600
chris-burd91 <chrisburd91@gmail.com>
2020-08-31 14:04:46 +0000

    changes to docker-compose

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,11 @@ export DATABASE_URI=${DATABASE_URI}
 export MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
 export TEST_DB_URI=${TEST_DB_URI}
 
+docker stack rm HTTPrequest
+docker-compose down
+
 docker service create --name registry --publish 4999:5000 registry
-docker-compose up -d --build && docker-compose down
+docker-compose build
 docker-compose push 
 docker stack deploy --compose-file docker-compose.yml HTTPrequest
+docker service ls
