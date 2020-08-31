c73130148c0fc55b1a6c58492f70a814652c18d4
chris-burd91 <chrisburd91@gmail.com>
2020-08-31 13:58:43 +0000

    exporting TEST_DB_URI

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 export APP_VERSION=1.0.0
 export DATABASE_URI=${DATABASE_URI}
 export MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
+export TEST_DB_URI=${TEST_DB_URI}
 
 docker service create --name registry --publish 4999:5000 registry
 docker-compose up -d --build && docker-compose down
